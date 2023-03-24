d9838fc74cade94a7724d746f5576c58f574bd71
minh1304 <vtm1304@gmail.com>
2023-03-24 13:05:27 +0700

    update

---
+++ src/app/controllers/SiteController.js
@@ -0,0 +1,12 @@
+class NewController {
+    //[GET] /home
+    home (req, res) {
+        res.render('home')
+    }
+    // [GET] /search
+    search (req, res) {
+        res.render('search')
+
+    }
+}
+module.exports = new NewController;
--- src/index.js
+++ src/index.js
@@ -5,9 +5,19 @@ const morgan = require('morgan')
 const app = express()
 const port = 3000
 
+const route = require('./routes')
 
 app.use(express.static(path.join(__dirname, 'public')))
-app.use(morgan('combined'))
+// app.use(morgan('combined'))
+
+
+
+//middleware
+app.use(express.urlencoded({extended: true}))
+app.use(express.json())
+
+
+
 
 app.engine('hbs', engine({
   extname: '.hbs'
@@ -15,12 +25,9 @@ app.engine('hbs', engine({
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources/views'));
 
-app.get('/', (req, res) => {
-  res.render('home')
-})
-app.get('/news', (req, res) => {
-  res.render('news')
-})
+
+//Routes init
+route(app)
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
